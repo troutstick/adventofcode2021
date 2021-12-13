8d73d081f1cfd6e4684973858fa01f1bafe25491
Justin Thein <justin.a.thein@gmail.com>
2021-12-12 18:05:10 -0800

    fin d3

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -49,30 +49,70 @@ pub fn sol(input: &String) {
 
         i32::from_str_radix(bin_str.as_str(), 2).unwrap()
     };
+    
+    let epsilon = !gamma & MASK;
 
-    let o2_rating = input.iter();
-    let co2_rating = input.iter();
+    println!("The answer to Day 3 Part 1 is {}", gamma * epsilon);
+    
+    let o2_rating = vec_to_bin(reduce_part2(copy_vect(&input), 0, true));
+    let co2_rating = vec_to_bin(reduce_part2(copy_vect(&input), 0, false));
+    println!("The answer to Day 3 Part 2 is {}", o2_rating * co2_rating);
+}
 
-    for i in 0..NUMS_WIDTH {
-        let is_most_common = |bin_input: &&Vec<usize>| {
-            bin_input[i] == if sums[i] >= input.len() / 2 {
-                1
-            } else {
-                0
-            }
-        };
-        let is_least_common = |bin_input: &&Vec<usize>| {
-            bin_input[i] == if sums[i] <= input.len() / 2 {
-                1
-            } else {
-                0
-            }
-        };
-        o2_rating = o2_rating.filter(is_most_common);
-        co2_rating = co2_rating.filter(is_least_common);
+fn vec_to_bin(bin_vec: Vec<usize>) -> usize {
+    let bin_value = |(index, val): (usize, &usize)| -> usize {
+        *val * (1 << index)
+    };
+    bin_vec.iter().rev().enumerate().map(bin_value).sum()
+}
+
+fn reduce_part2(list_nums: Vec<Vec<usize>>, index: usize, is_most_common: bool) -> Vec<usize> {
+    if index >= NUMS_WIDTH {
+        panic!("baddo");
     }
 
+    let num_to_match = get_most_or_least_common(&list_nums, index, is_most_common);
+    let list_nums = list_nums.iter()
+        .filter(|n| n[index] == num_to_match)
+        .map(|n| n.clone())
+        .collect::<Vec<Vec<usize>>>();
+    // println!("woo we have {} left", list_nums.len());
+    if list_nums.len() == 1 {
+        list_nums[0].clone()
+    } else {
+        reduce_part2(list_nums, index + 1, is_most_common)
+    }
+}
 
-    println!("The answer to Day 3 Part 1 is {}", gamma * (!gamma & MASK));
-    
+fn get_most_or_least_common(list_nums: &Vec<Vec<usize>>, index: usize, is_most_common: bool) -> usize {
+    let get_ith = |v: &Vec<usize>| -> usize {
+        v[index]
+    };
+
+    let filter_ones = |i: &usize| {
+        *i == 1
+    };
+    let num_ones = list_nums.iter().map(get_ith).filter(filter_ones).count();
+    let num_zeros = list_nums.len() - num_ones;
+    if num_ones >= num_zeros {
+       if is_most_common {
+           1
+       } else {
+           0
+       }
+    } else {
+        if is_most_common {
+            0
+        } else {
+            1
+        }
+    }
+}
+
+fn copy_vect(v: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut ret = Vec::with_capacity(v.len());
+    for bin_vec in v {
+        ret.push(bin_vec.clone());
+    }
+    ret
 }
