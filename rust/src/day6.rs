82d76fa0e00ed09fb1a64f13ec6e507332d3b817
Justin Thein <justin.a.thein@gmail.com>
2021-12-23 14:06:50 -0800

    fmt

--- rust/src/day6.rs
+++ rust/src/day6.rs
@@ -1,30 +1,33 @@
 pub fn sol(input: &String) {
     let mut age_to_num_fish = [0; 9];
-    let input = input
-        .trim()
-        .split(",")
-        .map(|s| s.parse::<usize>().unwrap());
-        
+    let input = input.trim().split(",").map(|s| s.parse::<usize>().unwrap());
+
     for age in input {
         age_to_num_fish[age] += 1;
     }
     for _ in 0..80 {
         age_to_num_fish = advance_ages(age_to_num_fish);
     }
-    println!("The answer to Day 6 Part 1 is {}", age_to_num_fish.iter().sum::<u64>());
-    
+    println!(
+        "The answer to Day 6 Part 1 is {}",
+        age_to_num_fish.iter().sum::<u64>()
+    );
+
     for _ in 80..256 {
         age_to_num_fish = advance_ages(age_to_num_fish);
     }
-    println!("The answer to Day 6 Part 2 is {}", age_to_num_fish.iter().sum::<u64>());
+    println!(
+        "The answer to Day 6 Part 2 is {}",
+        age_to_num_fish.iter().sum::<u64>()
+    );
 }
 
 fn advance_ages(prev_day: [u64; 9]) -> [u64; 9] {
-    let mut next_day = [0;9];
+    let mut next_day = [0; 9];
     for i in 1..prev_day.len() {
-        next_day[i-1] = prev_day[i];
+        next_day[i - 1] = prev_day[i];
     }
     next_day[6] += prev_day[0];
     next_day[8] += prev_day[0];
     next_day
-} 
+}
